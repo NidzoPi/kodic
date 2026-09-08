3c0b7c1ae1826dbfb0e79687ce4aaac12306252a
Nidzo Pi <pilipovicnikola68@gmail.com>
2026-09-08 14:17:36 +0200

    Add Yandex Metrica

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import YandexMetrica from "./components/YandexMetrica";
 
 import { Geist, Geist_Mono } from "next/font/google";
 
@@ -52,6 +53,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
+         <YandexMetrica />
         <ServiceWorkerRegistration />
         {children}
       </body>
